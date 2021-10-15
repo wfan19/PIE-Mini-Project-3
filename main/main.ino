3a4894aed804acae95e3893b9488f8197cbaf380
Benji Pugh <pughbenji@gmail.com>
2021-10-15 14:15:11 -0400

    Implemented bangBang and PID
    PID is actually currently PD.

--- main/main.ino
+++ main/main.ino
@@ -6,6 +6,9 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 
+int leftSens = A0;
+int rightSens = A1;
+
 int leftMotorVal = 0;
 int rightMotorVal = 0;
 
@@ -14,6 +17,14 @@ unsigned long tStart = 0;
 uint16_t cmd_buffer_pos = 0;
 const uint8_t CMD_BUFFER_LEN = 20;
 char cmd_buffer[CMD_BUFFER_LEN];
+
+
+// PD params
+float k_p = 0.1;
+float k_d = 0;
+int sensDiffPrev = 0;
+int tPrevious = 0;
+int baseSpeed = 30;
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -24,21 +35,22 @@ void setup() {
   }
   Serial.println("Motor Shield found.");
   tStart = millis();
+  tPrevious = tStart;
 }
 
 void loop() {
   if (Serial.available()) {
     char ch = Serial.read();
     
+    Serial.println(cmd_buffer);
+    
     if (ch == '\r') {
       Serial.println("New line detected");
       cmd_buffer[cmd_buffer_pos] = '\0';
       parseCommandBuffer();
       cmd_buffer_pos = 0;
-      memset(cmd_buffer, 0, sizeof(cmd_buffer));
     } else if (cmd_buffer_pos == CMD_BUFFER_LEN - 1) {
       cmd_buffer_pos = 0;
-      memset(cmd_buffer, 0, sizeof(cmd_buffer));
       cmd_buffer[cmd_buffer_pos] = ch;
       cmd_buffer_pos++;
     } else {
@@ -46,6 +58,9 @@ void loop() {
       cmd_buffer_pos++;
     }
   }
+
+  //bangBang();
+  pdControl();
   
   motorWrite();
   delay(50);
@@ -89,3 +104,33 @@ void motorWrite() {
   leftMotor->setSpeed(abs(leftMotorVal));
   rightMotor->setSpeed(abs(rightMotorVal));
 }
+
+void bangBang() {
+  int leftSensVal = analogRead(leftSens);
+  int rightSensVal = analogRead(rightSens);
+  int difference = leftSensVal - rightSensVal;
+
+  if (difference > 0) {
+    leftMotorVal = 0;
+    rightMotorVal = 128;
+    }
+  else {
+    leftMotorVal = -128;
+    rightMotorVal = 0;
+    }
+  
+}
+
+
+
+void pdControl() {
+  int leftSensVal = analogRead(leftSens);
+  int rightSensVal = analogRead(rightSens);
+  int sensDiff = leftSensVal - rightSensVal;
+  int tElapsed = millis() - tPrevious;
+  int motorDiff = k_p * sensDiff + k_d * (sensDiff-sensDiffPrev)/tElapsed;
+
+  leftMotorVal = -(baseSpeed + motorDiff);
+  rightMotorVal = baseSpeed - motorDiff;
+  sensDiffPrev = millis();
+  }
